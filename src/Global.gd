8bba51f56f1b2f25c48a45af688b176c4c6a4aba
kachow-UwU <116671973+kachow-UwU@users.noreply.github.com>
2022-12-15 10:57:12 +0400

    Add files via upload

---
+++ src/Global.gd
@@ -0,0 +1,45 @@
+extends Node
+
+const SAVE_FILE = "user://savegame.save"
+const ALL_TREASURES = 9
+
+
+signal update_treasure
+
+var player : Player
+var level: String = ""
+var gate: String = ""
+
+var curr_treasures : Dictionary = {}
+var treasures : Dictionary = {}
+var enemies : Dictionary = {}
+
+func _add_item(item: String, items: Dictionary):
+	if not level in items:
+		items[level] = [item]
+	elif not item in items[level]:
+		items[level].append(item)
+
+
+func add_tresure(treasure: String):
+	_add_item(treasure, curr_treasures)
+
+
+func _has_item(item: String, items: Dictionary) -> bool:
+	if level in items:
+		return item in items[level]
+	return false
+
+
+func has_treasure(treasure: String) -> bool:
+	return _has_item(treasure, treasures)
+
+
+
+func merge_treasures():
+	for level in curr_treasures:
+		if not level in treasures:
+			treasures[level] = curr_treasures[level]
+		else:
+			treasures[level] = treasures[level] + curr_treasures[level]
+	curr_treasures = {}
